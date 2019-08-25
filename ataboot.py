a8c1a4de1c024191e00be9cf086682e75d9428dd
Elliot Nunn <elliotnunn@me.com>
2019-08-25 21:59:07 +0800

    Definitive ATABoot patch
    Key realisations:
    
    1. The driver entry point only needs to be called once per disk,
       not once per bootable partition. Bootability is decided elsewhere.
    
    2. As a result, my shim code does not need to access the disk at all.
    
    3. None of the ROM-based ATADisk drivers support "slave" drives.
    
    4. Apple drivers (contrary to the Monster Disk Driver Technote) only care
       about D5 (the drive ID and some flags) on entry.

--- ataboot.py
+++ ataboot.py
@@ -11,26 +11,11 @@ import binascii
 
 
 src, cleanup = patch_common.get_src(desc='''
-Enable booting from ATA disks without an Apple_Driver_ATA partition. This is
-done by patching the ROM-based .ATALoad DRVR to fall back on the ROM-based
-.ATADisk DRVR when initially scanning a disk. The .ATADisk is added to the ROM
-if missing. The disk still needs an Apple Partition Map.
+Support ATA startup disks without an Apple_Driver_ATA partition. This allows Mac
+OS to be installed on a disk without repartitioning it. Accomplished by
+shoehorning the Drive Setup 2.1 ATA driver into the ROM ATALoad driver, to
+replace missing driver partitions.
 ''')
-
-
-# long BOOT(void *DRVR, void *PartMapEntry, long flags)
-# C calling convention: args pushed in reverse order and not popped by function, return value in d0
-def find_BOOT(code):
-    for i in range(0, len(code), 2):
-        if code[i:i+4] == b'BOOT': # MOVE.L 'BOOT',D0  --or-- MOVE.L 'BOOT',-(SP)
-            for j in reversed(range(0, i, 2)):
-                # Count backwards and look for the target of *any* JSR, which is the start of the func
-                for k in range(0, len(code), 2):
-                    if code[k:k+2] == b'\x4E\xBA': # JSR opcode
-                        if k+2 + struct.unpack('>h', code[k+2:k+4])[0] == j:
-                            return j
-
-    raise ValueError('Function BOOT not found')
 
 
 def find_InitDevice(code):
@@ -46,7 +31,7 @@ def find_InitDevice(code):
 
 def patch_ataload(code):
     code = bytearray(code)
-    new = len(code) # boundary between old and new code
+    cut1 = len(code) # boundary between original and glue
 
     # Parse the DumpObj'd file
     with open(path.join(path.dirname(__file__), 'ATALoad.dmp')) as f:
@@ -55,46 +40,50 @@ def patch_ataload(code):
             if m:
                 code.extend(binascii.unhexlify(m.group(1).replace(' ', '')))
 
-    InitDevice = find_InitDevice(code[:new])
-    BOOT = find_BOOT(code[:new])
+    cut2 = len(code) # boundary between glue and driver
 
-    print('ATALoad patch: InitDevice=0x%X, NewInitDevice=0x%X, BOOT=0x%X' % (InitDevice, new, BOOT))
+    with open(path.join(path.dirname(__file__), 'AppleATADisk'), 'rb') as f:
+        code.extend(f.read())
+
+    InitDevice = find_InitDevice(code[:cut1])
+
+    print('ATALoad patch: InitDevice=0x%X, glue=0x%X, driver=0x%X' % (InitDevice, cut1, cut2))
 
     # "Link" the new code into the old
-    for i in range(new, len(code), 2):
+    for i in range(cut1, len(code), 2):
         if code[i:i+4] == b'Nsrt':
             code[i:i+4] = code[InitDevice:InitDevice+4] # copy the first LINK from the original function
             code[InitDevice:InitDevice+2] = b'\x60\x00' # BRA.W opcode, to...
-            code[InitDevice+2:InitDevice+4] = struct.pack('>h', new - (InitDevice+2)) # ...the start of the new code
+            code[InitDevice+2:InitDevice+4] = struct.pack('>h', cut1 - (InitDevice+2)) # ...the start of the new code
+
+        if code[i:i+2] == b'At':
+            code[i:i+2] = struct.pack('>h', cut2 - i)
+
+        if code[i:i+4] == b'Size':
+            code[i:i+4] = struct.pack('>l', cut2 - cut1)
 
         if code[i:i+2] == b'ID': # reference to original InitDevice, skipping the mangled 4-byte LINK
             code[i:i+2] = struct.pack('>h', (InitDevice+4) - i)
 
-        if code[i:i+2] == b'BT': # reference to BOOT function
-            code[i:i+2] = struct.pack('>h', BOOT - i)
-
     return bytes(code)
 
+
+found_drvr = False
 
 for (parent, folders, files) in os.walk(src):
     folders.sort(); files.sort() # make it kinda deterministic
     for filename in files:
         full = path.join(src, parent, filename)
 
-        if filename == 'Romfile':
-            ataload = path.join(src, parent, 'Rsrc', 'DRVR_-20175_ATALoad')
-            atadisk = path.join(src, parent, 'Rsrc', 'DRVR_53_ATADisk')
+        if filename == 'DRVR_-20175_ATALoad':
+            code = open(full, 'rb').read()
+            code = patch_ataload(code)
+            open(full, 'wb').write(code)
+            found_drvr = True
 
-            if path.exists(ataload):
-                code = open(ataload, 'rb').read()
-                code = patch_ataload(code)
-                open(ataload, 'wb').write(code)
 
-                if not path.exists(atadisk):
-                    with open(full, 'a') as f: # append to Romfile
-                        f.write('\ntype=DRVR   id=53       name=.ATADisk           src=Rsrc/DRVR_53_ATADisk # inserted by ataboot.py\n')
-                print('ATALoad patch: using .ATADisk DRVR from Drive Setup 2.1')
-                shutil.copy(path.join(path.dirname(__file__), 'DRVR_53_ATADisk'), atadisk)
+if not found_drvr:
+    raise ValueError('ATALoad DRVR not found')
 
 
 cleanup()
